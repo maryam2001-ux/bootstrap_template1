ea5928dc17dad11d69ca64a9225fc293f4ff79ae
maryam <mariamshrif06@gmail.com>
2025-03-16 12:52:34 +0200

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,10 @@
+
+// *Setup and start animation!  for the typing effect
+
+var typed = new Typed('.typing', {
+  strings: ["developer" , "designer" , " photographer" , " freelancer"],
+  typeSpeed: 150,
+  backSpeed: 100,
+  loop: true
+
+});           
